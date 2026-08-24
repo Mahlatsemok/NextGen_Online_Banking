00abfbbd0dde30061358ced511ae21d212abaa3c
Mahlatse Mokhabela <mamokswgc025@student.wethinkcode.co.za>
2026-08-24 15:06:04 +0200

    chore: initialize NextGen online banking project

---
+++ src/main/java/com/nextgen/onlinebanking/NextGenOnlineBankingApplication.java
@@ -0,0 +1,13 @@
+package com.nextgen.onlinebanking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NextGenOnlineBankingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NextGenOnlineBankingApplication.class, args);
+	}
+
+}
