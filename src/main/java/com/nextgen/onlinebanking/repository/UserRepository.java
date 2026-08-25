366de92b16549dabf54fd7e1c87909e0a0d155b8
Mahlatse Mokhabela <mamokswgc025@student.wethinkcode.co.za>
2026-08-25 10:08:04 +0200

    feat: add UserRepository for user persistence

---
+++ src/main/java/com/nextgen/onlinebanking/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package com.nextgen.onlinebanking.repository;
+
+import com.nextgen.onlinebanking.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByEmail(String email);
+
+    boolean existsByEmail(String email);
+}
