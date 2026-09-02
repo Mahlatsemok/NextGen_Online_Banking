7d06914f2df839dbc8e54eb6600f53e6a19082ff
Mahlatse Mokhabela <mamokswgc025@student.wethinkcode.co.za>
2026-09-02 13:17:48 +0200

    feat: add authentication response DTO

---
+++ src/main/java/com/nextgen/onlinebanking/dto/AuthenticationResponse.java
@@ -0,0 +1,48 @@
+package com.nextgen.onlinebanking.dto;
+
+import com.nextgen.onlinebanking.model.User;
+
+public class AuthenticationResponse {
+
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String email;
+
+    public AuthenticationResponse(
+            Long id,
+            String firstName,
+            String lastName,
+            String email) {
+
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+    }
+
+    public static AuthenticationResponse fromUser(User user) {
+        return new AuthenticationResponse(
+                user.getId(),
+                user.getFirstName(),
+                user.getLastName(),
+                user.getEmail()
+        );
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+}
