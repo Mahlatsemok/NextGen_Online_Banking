48dd298e6b279faf857be88ae5c2efc2288a4f1a
Mahlatse Mokhabela <mamokswgc025@student.wethinkcode.co.za>
2026-09-02 14:37:06 +0200

    feat: add invalid credentials exception

---
+++ src/main/java/com/nextgen/onlinebanking/exception/InvalidCredentialsException.java
@@ -0,0 +1,8 @@
+package com.nextgen.onlinebanking.exception;
+
+public class InvalidCredentialsException extends RuntimeException {
+
+    public InvalidCredentialsException(String message) {
+        super(message);
+    }
+}
